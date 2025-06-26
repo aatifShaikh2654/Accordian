8ce4a74907dd5c17b211ecba57b4a18aed593673
Aatif Shaikh <atif@webify.ai>
2025-06-26 11:12:10 +0530

    aatif 26-6-25

--- src/app/components/Accordian.tsx
+++ src/app/components/Accordian.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import { contentType } from '../Home'
 import styles from '../accordian.module.css'
 import { LiaAngleDownSolid } from "react-icons/lia";
